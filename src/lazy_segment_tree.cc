777b529462eef0873326f37b364f51b7311d5707
M-Y-Sun <sun.m.yixuan@gmail.com>
2024-09-04 11:45:06 -0700

    separate both segment trees into header and source

--- src/lazy_segment_tree.cc
+++ src/lazy_segment_tree.cc
@@ -6,211 +6,206 @@
 #include <functional>
 #include <vector>
 
-/** A data structure that can answer point update and range maximum queries. */
-template <typename T> class lazy_segment_tree
+#include "../include/tree.hh"
+
+namespace ext
 {
-private:
-    struct node_t_ {
-        T val;
-        T lz_add;
-        T lz_set;
-    };
 
-    const T DEFAULT_;
+namespace lz_segtr
+{
 
-    // tree[1] is the top node, tree[2] and tree[3] are its children, and so on
-    // values from tree[len] to tree[2 * len - 1] is the original array
-    // that is:
-    // the parent of tree[i] is tree[i / 2]
-    // and its children are tree[2i] and tree[2i + 1]
+template <typename T>
+T
+tree<T>::next_p2_ (T v)
+{
+    --v;
+    v |= v >> 1;
+    v |= v >> 2;
+    v |= v >> 4;
+    v |= v >> 8;
+    v |= v >> 16;
 
-    std::vector<node_t_> tree_; // length of this is 2 * len
-    size_t               len_;  // simulated "length" of the array
+    return v + 1;
+}
 
-    std::function<T (const T &, const T &)> queryfunc;
+template <typename T>
+inline void
+tree<T>::pushup_ (size_t pos)
+{
+    const T lc     = pos << 1;
+    tree_[pos].val = tree_[lc].val + tree_[lc + 1].val;
+}
 
-protected:
-    T
-    next_p2_ (T v)
-    {
-        --v;
-        v |= v >> 1;
-        v |= v >> 2;
-        v |= v >> 4;
-        v |= v >> 8;
-        v |= v >> 16;
+template <typename T>
+void
+tree<T>::pushdown_ (size_t pos, size_t lb, size_t mid, size_t rb)
+{
+    const T lc = pos << 1;
+    const T rc = lc + 1;
 
-        return v + 1;
-    }
+    if (tree_[pos].lz_set != 0) { // range set
+        tree_[lc].lz_set = tree_[rc].lz_set = tree_[pos].lz_set;
 
-    inline void
-    pushup_ (size_t pos)
-    {
-        const T lc     = pos << 1;
-        tree_[pos].val = tree_[lc].val + tree_[lc + 1].val;
-    }
+        tree_[lc].val = (mid - lb + 1) * tree_[pos].lz_set;
+        tree_[rc].val = (rb - mid) * tree_[pos].lz_set;
 
-    void
-    pushdown_ (size_t pos, size_t lb, size_t mid, size_t rb)
-    {
-        const T lc = pos << 1;
-        const T rc = lc + 1;
-
-        if (tree_[pos].lz_set != 0) { // range set
-            tree_[lc].lz_set = tree_[rc].lz_set = tree_[pos].lz_set;
-
-            tree_[lc].val = (mid - lb + 1) * tree_[pos].lz_set;
-            tree_[rc].val = (rb - mid) * tree_[pos].lz_set;
-
-            tree_[lc].lz_add  = 0;
-            tree_[rc].lz_add  = 0;
-            tree_[pos].lz_set = 0;
-        } else if (tree_[pos].lz_add != 0) { // range add
-            if (tree_[lc].lz_set == 0) {
-                tree_[lc].lz_add += tree_[pos].lz_add;
-            } else {
-                tree_[lc].lz_set += tree_[pos].lz_add;
-                tree_[lc].lz_add = 0;
-            }
-
-            if (tree_[rc].lz_set == 0) {
-                tree_[rc].lz_add += tree_[pos].lz_add;
-            } else {
-                tree_[rc].lz_set += tree_[pos].lz_add;
-                tree_[rc].lz_add = 0;
-            }
-
-            tree_[lc].val += (mid - lb + 1) * tree_[pos].lz_add;
-            tree_[rc].val += (rb - mid) * tree_[pos].lz_add;
-            tree_[pos].lz_add = 0;
-        }
-    }
-
-    void
-    build_ (int pos, int lb, int rb, const std::vector<T> &arr)
-    {
-        // clear lazy fields
-        tree_[pos].lz_add = 0;
+        tree_[lc].lz_add  = 0;
+        tree_[rc].lz_add  = 0;
         tree_[pos].lz_set = 0;
-
-        // once we are at a leaf, set the value from the source array
-        if (lb == rb) {
-            tree_[pos].val = arr[lb];
-            return;
+    } else if (tree_[pos].lz_add != 0) { // range add
+        if (tree_[lc].lz_set == 0) {
+            tree_[lc].lz_add += tree_[pos].lz_add;
+        } else {
+            tree_[lc].lz_set += tree_[pos].lz_add;
+            tree_[lc].lz_add = 0;
         }
 
-        T mid = lb + ((rb - lb) >> 1);
-        T lc  = pos << 1;
-        build_ (lc, lb, mid, arr);
-        build_ (lc + 1, mid + 1, rb, arr);
-
-        pushup_ (pos);
-    }
-
-public:
-    lazy_segment_tree (
-        size_t len, T dft, std::vector<T> arr,
-        std::function<T (const T &, const T &)> const &queryfunc)
-        : DEFAULT_ (dft), tree_ (next_p2_ (len) << 1, { dft, 0, 0 }),
-          len_ (len), queryfunc (queryfunc)
-    {
-        build_ (1, 1, len, arr);
-    }
-
-    /** Adds a value to a range of elements. */
-    void
-    add (size_t start, size_t end, T val, size_t pos_, size_t lb_, size_t rb_)
-    {
-        // if [lb, rb] and [start, end] do not intersect at all, do nothing
-        if (start > rb_ || end < lb_)
-            return;
-
-        // if [lb, rb] is completely inside [start, end], set the current node
-        // and return
-        if (start <= lb_ && rb_ <= end) {
-            // if the lower and upper bounds is completely inside [start, end]
-            tree_[pos_].val += (rb_ - lb_ + 1) * val;
-
-            // add to lazy propagation if the lazy set value is 0
-            if (tree_[pos_].lz_set == 0)
-                tree_[pos_].lz_add += val;
-            else
-                tree_[pos_].lz_set += val;
-
-            return;
+        if (tree_[rc].lz_set == 0) {
+            tree_[rc].lz_add += tree_[pos].lz_add;
+        } else {
+            tree_[rc].lz_set += tree_[pos].lz_add;
+            tree_[rc].lz_add = 0;
         }
 
-        // if [lb, rb] only partially intersects with [start, end], continue
-        // searching until something happens
+        tree_[lc].val += (mid - lb + 1) * tree_[pos].lz_add;
+        tree_[rc].val += (rb - mid) * tree_[pos].lz_add;
+        tree_[pos].lz_add = 0;
+    }
+}
 
-        size_t mid = lb_ + ((rb_ - lb_) >> 1);
-        pushdown_ (pos_, lb_, mid, rb_);
+template <typename T>
+void
+tree<T>::build_ (int pos, int lb, int rb, const std::vector<T> &arr)
+{
+    // clear lazy fields
+    tree_[pos].lz_add = 0;
+    tree_[pos].lz_set = 0;
 
-        // search the left and right children
-        size_t lc = pos_ << 1;
-        add (start, end, val, lc, lb_, mid);
-        add (start, end, val, lc + 1, mid + 1, rb_);
-
-        pushup_ (pos_);
+    // once we are at a leaf, set the value from the source array
+    if (lb == rb) {
+        tree_[pos].val = arr[lb];
+        return;
     }
 
-    /** Sets the value to a range of elements to another value. */
-    void
-    set (size_t start, size_t end, T val, size_t pos_, size_t lb_, size_t rb_)
-    {
-        // if [lb, rb] and [start, end] do not intersect at all, do nothing
-        if (start > rb_ || end < lb_)
-            return;
+    T mid = lb + ((rb - lb) >> 1);
+    T lc  = pos << 1;
+    build_ (lc, lb, mid, arr);
+    build_ (lc + 1, mid + 1, rb, arr);
 
-        // if [lb, rb] is completely inside [start, end], set the current node
-        // and return
-        if (start <= lb_ && end >= rb_) {
-            tree_[pos_].val    = (rb_ - lb_ + 1) * val;
-            tree_[pos_].lz_add = 0;
-            tree_[pos_].lz_set = val;
-            return;
-        }
+    pushup_ (pos);
+}
 
-        // if [lb, rb] only partially intersects with [start, end], continue
-        // searching until something happens
+template <typename T>
+tree<T>::tree (size_t len, T dft, std::vector<T> arr,
+               std::function<T (const T &, const T &)> const &queryfunc)
+    : DEFAULT_ (dft), tree_ (next_p2_ (len) << 1, { dft, 0, 0 }), len_ (len),
+      queryfunc (queryfunc)
+{
+    build_ (1, 1, len, arr);
+}
 
-        size_t mid = lb_ + ((rb_ - lb_) >> 1);
-        pushdown_ (pos_, lb_, mid, rb_);
+/** Adds a value to a range of elements. */
+template <typename T>
+void
+tree<T>::add (size_t start, size_t end, T val, size_t pos_, size_t lb_,
+              size_t rb_)
+{
+    // if [lb, rb] and [start, end] do not intersect at all, do nothing
+    if (start > rb_ || end < lb_)
+        return;
 
-        // search the left and right children
-        size_t lc = pos_ << 1;
-        set (start, end, val, lc, lb_, mid);
-        set (start, end, val, lc + 1, mid + 1, rb_);
+    // if [lb, rb] is completely inside [start, end], set the current node
+    // and return
+    if (start <= lb_ && rb_ <= end) {
+        // if the lower and upper bounds is completely inside [start, end]
+        tree_[pos_].val += (rb_ - lb_ + 1) * val;
 
-        pushup_ (pos_);
+        // add to lazy propagation if the lazy set value is 0
+        if (tree_[pos_].lz_set == 0)
+            tree_[pos_].lz_add += val;
+        else
+            tree_[pos_].lz_set += val;
+
+        return;
     }
 
-    /** @return Queries the range [start, end) recursively with O(log n) time
-     * complexity.*/
-    T
-    query (size_t start, size_t end, size_t pos_, size_t lb_, size_t rb_)
-    {
-        // if [lb, rb] and [start, end] do not intersect at all, return dft
-        if (start > rb_ || end < lb_)
-            return DEFAULT_;
+    // if [lb, rb] only partially intersects with [start, end], continue
+    // searching until something happens
 
-        // if [lb, rb] is completely inside [start, end], return the current
-        // value
-        if (start <= lb_ && end >= rb_)
-            return tree_[pos_].val;
+    size_t mid = lb_ + ((rb_ - lb_) >> 1);
+    pushdown_ (pos_, lb_, mid, rb_);
 
-        // if it is partially inside, continue the search to the left and right
-        // children
-        size_t mid = lb_ + ((rb_ - lb_) >> 1);
-        pushdown_ (pos_, lb_, mid, rb_);
+    // search the left and right children
+    size_t lc = pos_ << 1;
+    add (start, end, val, lc, lb_, mid);
+    add (start, end, val, lc + 1, mid + 1, rb_);
 
-        size_t lc = pos_ << 1;
+    pushup_ (pos_);
+}
 
-        // search the left and right half
-        return queryfunc (query (start, end, lc, lb_, mid),
-                          query (start, end, lc + 1, mid + 1, rb_));
+/** Sets the value to a range of elements to another value. */
+template <typename T>
+void
+tree<T>::set (size_t start, size_t end, T val, size_t pos_, size_t lb_,
+              size_t rb_)
+{
+    // if [lb, rb] and [start, end] do not intersect at all, do nothing
+    if (start > rb_ || end < lb_)
+        return;
+
+    // if [lb, rb] is completely inside [start, end], set the current node
+    // and return
+    if (start <= lb_ && end >= rb_) {
+        tree_[pos_].val    = (rb_ - lb_ + 1) * val;
+        tree_[pos_].lz_add = 0;
+        tree_[pos_].lz_set = val;
+        return;
     }
-};
+
+    // if [lb, rb] only partially intersects with [start, end], continue
+    // searching until something happens
+
+    size_t mid = lb_ + ((rb_ - lb_) >> 1);
+    pushdown_ (pos_, lb_, mid, rb_);
+
+    // search the left and right children
+    size_t lc = pos_ << 1;
+    set (start, end, val, lc, lb_, mid);
+    set (start, end, val, lc + 1, mid + 1, rb_);
+
+    pushup_ (pos_);
+}
+
+/** @return Queries the range [start, end) recursively with O(log n) time
+ * complexity.*/
+template <typename T>
+T
+tree<T>::query (size_t start, size_t end, size_t pos_, size_t lb_, size_t rb_)
+{
+    // if [lb, rb] and [start, end] do not intersect at all, return dft
+    if (start > rb_ || end < lb_)
+        return DEFAULT_;
+
+    // if [lb, rb] is completely inside [start, end], return the current
+    // value
+    if (start <= lb_ && end >= rb_)
+        return tree_[pos_].val;
+
+    // if it is partially inside, continue the search to the left and right
+    // children
+    size_t mid = lb_ + ((rb_ - lb_) >> 1);
+    pushdown_ (pos_, lb_, mid, rb_);
+
+    size_t lc = pos_ << 1;
+
+    // search the left and right half
+    return queryfunc (query (start, end, lc, lb_, mid),
+                      query (start, end, lc + 1, mid + 1, rb_));
+}
+
+} // namespace lz_segtr
+
+} // namespace ext
 
 int
 main ()
@@ -224,7 +219,7 @@ main ()
     for (int i = 1; i <= n; ++i)
         scanf ("%lld", &arr[i]);
 
-    lazy_segment_tree<int64_t> segtree (
+    ext::lz_segtr::tree<int64_t> segtree (
         n, 0, arr, [] (const int64_t &x, const int64_t &y) { return x + y; });
 
     while (q--) {
--- src/segment_tree.cc
+++ src/segment_tree.cc
@@ -4,77 +4,70 @@
 #include <climits>
 #include <cstdio>
 #include <functional>
-#include <vector>
 
-/** A data structure that can answer point update and range maximum queries. */
-template <typename T> class segment_tree
+#include "../include/tree.hh"
+
+namespace ext
 {
-private:
-    const T DEFAULT_;
 
-    // tree[1] is the top node, tree[2] and tree[3] are its children, and so on
-    // values from tree[len] to tree[2 * len - 1] is the original array
-    // that is:
-    // the parent of tree[i] is tree[i / 2]
-    // and its children are tree[2i] and tree[2i + 1]
+namespace segtr
+{
 
-    std::vector<T> tree_; // length of this is 2 * len
-    size_t         len_;  // simulated "length" of the array
+template <typename T>
+tree<T>::tree (size_t len, const T dft,
+               std::function<T (const T &, const T &)> const &queryfunc)
+    : DEFAULT_ (dft), tree_ (len << 1, dft), len_ (len), queryfunc (queryfunc)
+{
+}
 
-    std::function<T (const T &, const T &)> queryfunc;
+/** Sets the value at id to val */
+template <typename T>
+void
+tree<T>::set (int idx, T val)
+{
+    idx += len_;
+    tree_[idx] = val;
 
-public:
-    segment_tree (size_t len, T dft,
-                  std::function<T (const T &, const T &)> const &queryfunc)
-        : DEFAULT_ (dft), tree_ (len << 1, dft), len_ (len),
-          queryfunc (queryfunc)
-    {
+    // start at the bottom of the tree and update nodes until root
+    for (; idx > 1; idx >>= 1)
+        tree_[idx >> 1] = queryfunc (tree_[idx], tree_[idx ^ 1]);
+}
+
+/** @return A query of the range [start, end) */
+template <typename T>
+T
+tree<T>::query (size_t start, size_t end)
+{
+    T ans = DEFAULT_;
+    for (start += len_, end += len_; start < end; start >>= 1, end >>= 1) {
+        // for each level, process the values that do not belong to the
+        // higher range then move the range one level higher
+
+        // if it is left child => not part of the [start, end] range, it
+        // should be processed
+        if (start & 1)
+            ans = queryfunc (ans, tree_[start++]);
+
+        // if it is left child => part of the [start, end] range, the right
+        // child (previous index) should be processed
+        if (end & 1)
+            ans = queryfunc (ans, tree_[--end]);
     }
 
-    /** Sets the value at id to val */
-    void
-    set (int idx, T val)
-    {
-        idx += len_;
-        tree_[idx] = val;
+    return ans;
+}
 
-        // start at the bottom of the tree and update nodes until root
-        for (; idx > 1; idx >>= 1)
-            tree_[idx >> 1] = queryfunc (tree_[idx], tree_[idx ^ 1]);
-    }
+} // namespace segtr
 
-    /** @return A query of the range [start, end) */
-    T
-    query (size_t start, size_t end)
-    {
-        T ans = DEFAULT_;
-        for (start += len_, end += len_; start < end; start >>= 1, end >>= 1) {
-            // for each level, process the values that do not belong to the
-            // higher range then move the range one level higher
+} // namespace ext
 
-            // if it is left child => not part of the [start, end] range, it
-            // should be processed
-            if (start & 1)
-                ans = queryfunc (ans, tree_[start++]);
-
-            // if it is left child => part of the [start, end] range, the right
-            // child (previous index) should be processed
-            if (end & 1)
-                ans = queryfunc (ans, tree_[--end]);
-        }
-
-        return ans;
-    }
-};
-
-/*
 int
 main ()
 {
     int n, q;
     scanf ("%d%d", &n, &q);
 
-    segment_tree<int64_t> segtree (
+    ext::segtr::tree<int64_t> segtree (
         n, INT64_MAX,
         [] (const int64_t &x, const int64_t &y) { return std::min (x, y); });
 
@@ -100,4 +93,3 @@ main ()
 
     return 0;
 }
-*/
